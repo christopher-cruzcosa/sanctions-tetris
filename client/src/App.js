2457784ce9616d48553f6d25f7132be67a34ce83
Chris C <christopher.cruzcosa@gmail.com>
2022-02-08 20:30:15 -0800

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./components/HomePage";
+import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
+import Navbar from "./components/Navbar";
+import ProtectedExamplePage from "./components/ProtectedExamplePage";
+import { ProvideAuth } from "./util/authContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Tetris from "./components/Tetris";
+
+function App() {
+  return (
+    <ProvideAuth>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Tetris />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/signup">
+            <SignupPage />
+          </Route>
+          <ProtectedRoute path="/protected/example">
+            <ProtectedExamplePage />
+          </ProtectedRoute>
+        </Switch>
+      </Router>
+    </ProvideAuth>
+  );
+}
+
+export default App;
